0f1afd0b9128f7cf718857cdb214a6b72ca06a33
Paige Goldhagen <60744834+paigegoldhagen@users.noreply.github.com>
2023-11-18 16:37:04 +1100

    v1.2.1 - code refactor

--- src/main/java/com/paigegoldhagen/astral/EventsHandler.java
+++ src/main/java/com/paigegoldhagen/astral/EventsHandler.java
@@ -5,37 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * For creating the events timetable and retrieving upcoming events.
+ * For calculating projected event times and getting the upcoming events list.
  */
 public class EventsHandler {
-    /**
-     * Calculate projected event times and add the updated events to the events timetable.
-     *
-     * @param eventsList        the Events class as a list of beans
-     * @return                  the events timetable with each event name, location, and projected event times
-     */
-    public static List<Object[]> getEventsTimetable(List<Events> eventsList) {
-        List<Object[]> eventsTimetable = new ArrayList<>();
-
-        for (Events event : eventsList) {
-            String bossName = event.getName();
-            String eventLocation = event.getLocation();
-            List<LocalTime> projectedEventTimes = getProjectedEventTimes(event);
-
-            Object[] eventLine = new Object[]{bossName, eventLocation, projectedEventTimes};
-            eventsTimetable.add(eventLine);
-        }
-        return eventsTimetable;
-    }
-
     /**
      * Get the scheduled event times or calculate the projected event times over a 24-hour period
      * using the initial start time and the frequency of the event.
      *
-     * @param event         the event data to get the event schedule or initial start time and frequency
-     * @return              a list of LocalTime times associated with an event
+     * @param event     the event data to get the event schedule or initial start time and frequency
+     * @return          a list of LocalTime times associated with an event
      */
-    private static List<LocalTime> getProjectedEventTimes(Events event) {
+    public static List<LocalTime> getProjectedEventTimes(Events event) {
         List<LocalTime> projectedEventTimes = new ArrayList<>();
         String[] eventSchedule = event.getSchedule();
 
@@ -60,41 +40,42 @@ public class EventsHandler {
     }
 
     /**
-     * Store upcoming events in a list if the event notification time matches UTC time
+     * Store upcoming events in the UpcomingEvents class if the event notification time matches UTC time
      * and if notifications for the event are enabled.
      *
-     * @param eventsTimetable       the list of events with name, location, and projected event times
-     * @param timeChoice            the user choice for the notification reminder in minutes
-     * @param eventChoices          the user choices for receiving notifications per event
-     * @param utcTime               the current time in UTC
+     * @param timetable         the list of events with name, location, and projected event times
+     * @param notifyTime        the user choice for the notification reminder in minutes
+     * @param notifyState       the user choices for receiving notifications per event
+     * @param utcTime           the current time in UTC
      *
-     * @return                      a list of upcoming events
+     * @return                  a list of upcoming events
      */
-    public static List<Object[]> getUpcomingEvents(List<Object[]> eventsTimetable, String timeChoice, List<String> eventChoices, LocalTime utcTime) {
-        List<Object[]> upcomingEvents = new ArrayList<>();
-        int minutesBeforeEvent = Integer.parseInt(timeChoice);
-
-        for (Object[] event : eventsTimetable) {
-            int indexOfEvent = eventsTimetable.indexOf(event);
-
-            String bossName = (String) event[0];
-            String eventLocation = (String) event[1];
-            List<LocalTime> projectedEventTimes = (List<LocalTime>) event[2];
-
-            for (LocalTime time : projectedEventTimes) {
-                time = time.minusMinutes(minutesBeforeEvent);
-
-                if (time.equals(utcTime)) {
-                    String notificationChoice = eventChoices.get(indexOfEvent);
-
-                    if (notificationChoice.equals("1")) {
-                        Object[] eventDetails = new Object[]{bossName, eventLocation};
-                        upcomingEvents.add(eventDetails);
-                        break;
-                    }
-                }
-            }
+	public static List<UpcomingEvents> getUpcomingEvents(List<Timetable> timetable, String notifyTime, String notifyState, LocalTime utcTime) {
+        List<String> stateList = new ArrayList<>();
+        for (int i = 0; i < notifyState.length(); i++) {
+            String state = String.valueOf(notifyState.charAt(i));
+            stateList.add(state);
         }
-        return upcomingEvents;
-    }
+
+        List<UpcomingEvents> upcomingEvents = new ArrayList<>();
+		for (Timetable t : timetable) {
+			int indexOfEvent = timetable.indexOf(t);
+			List<LocalTime> eventTimes = t.getTimes();
+			
+			for (LocalTime time : eventTimes) {
+				time = time.minusMinutes(Integer.parseInt(notifyTime));
+				
+				if (time.equals(utcTime)) {
+					String indexOfState = stateList.get(indexOfEvent);
+					
+					if (indexOfState.equals("1")) {
+						UpcomingEvents event = new UpcomingEvents(t.getName(), t.getLocation());
+						upcomingEvents.add(event);
+						break;
+					}
+				}
+			}
+		}
+		return upcomingEvents;
+	}
 }
--- src/main/java/com/paigegoldhagen/astral/GUI.java
+++ src/main/java/com/paigegoldhagen/astral/GUI.java
@@ -3,86 +3,41 @@ package com.paigegoldhagen.astral;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.prefs.Preferences;
 
 /**
- * The main loop and the graphical user interface for the app.
+ * The graphical user interface for the app.
  */
 public class GUI {
-    private static String timeChoice;
-    private static List<String> eventChoices;
+	private static Preferences prefs;
 
-    /**
-     * Prepare the tray icon, get the event data and the default/saved user preferences,
-     * display the GUI, and send a notification if there is an upcoming event.
-     * Pause for 1 second or 1 minute at the end of every loop.
-     *
-     * @throws InterruptedException     the current thread was interrupted
-     * @throws IOException              the file couldn't be found or read
-     * @throws AWTException             an error occurred with the GUI components or window frame
-     */
-    public static void main(String[] args) throws InterruptedException, IOException, AWTException {
-        BufferedImage appIcon = FileHandler.loadImage("icon.png");
-        TrayIcon trayIcon = TrayHandler.prepareTrayIcon(appIcon);
+	/**
+	 * Display the GUI components and add GUI component listeners for user input.
+	 *
+	 * @param defaultPreferences		the default state preferences
+	 * @param timetable					the list of events with name, location, and projected event times
+	 */
+	public static void initialiseGUI(String defaultPreferences, List<Timetable> timetable) {
+        prefs = Preferences.userNodeForPackage(GUI.class);
+        String notifyTime = getNotifyTime();
+		String notifyState = getNotifyState(defaultPreferences);
 
-        InputStream csvFile = FileHandler.getInputStream("event-data.csv");
-        List<Events> eventsList = FormatHandler.mapCsvToBeans(csvFile);
-        List<Object[]> eventsTimetable = EventsHandler.getEventsTimetable(eventsList);
-
-        timeChoice = PreferenceHandler.getTimePreference();
-        String defaultEventPreferences = FormatHandler.calculateDefaultEventPreferences(eventsTimetable);
-        String eventPreferences = PreferenceHandler.getEventPreferences(defaultEventPreferences);
-        eventChoices = FormatHandler.getEventPreferencesAsList(eventPreferences);
-
-        List<Object[]> checkboxParameters = FormatHandler.getCheckboxParameters(eventsTimetable, eventChoices);
-        initialiseGUI(checkboxParameters);
-
-        while (true) {
-            PreferenceHandler.setTimePreference(timeChoice);
-            String eventPreferencesString = FormatHandler.getEventPreferencesAsString(eventChoices);
-            PreferenceHandler.setEventPreferences(eventPreferencesString);
-
-            LocalTime utcTime = TimeHandler.getUtcTime();
-            long millisecondsToWait = 1000;
-
-            List<Object[]> upcomingEvents = EventsHandler.getUpcomingEvents(eventsTimetable, timeChoice, eventChoices, utcTime);
-
-            if (!upcomingEvents.isEmpty()) {
-                String notificationMessage = FormatHandler.getNotificationMessage(upcomingEvents, timeChoice);
-                Notifications.sendNotification(trayIcon, notificationMessage);
-                millisecondsToWait = 60000;
-            }
-            Thread.sleep(millisecondsToWait);
-        }
-    }
-
-    /**
-     * Display the GUI components and add GUI component listeners for user input.
-     *
-     * @param checkboxParameters        the list of parameters to initialise the checkbox contents
-     */
-    private static void initialiseGUI(List<Object[]> checkboxParameters) {
-        Frame frame = new Frame();
+		Frame frame = new Frame();
         Panel panel = new Panel();
-
-        List<Checkbox> checkboxComponents = addCheckboxComponents(panel, checkboxParameters);
-
-        Component[] guiComponents = loadGUIComponents(panel);
-        Choice dropdown = (Choice) guiComponents[3];
+		
+		GUIComponents comp = loadGUIComponents(panel, timetable, notifyState);
+		Choice dropdown = comp.getDropdown();
 
         String[] timeChoices = new String[]{"5", "10", "15", "20", "30", "60"};
-
         for (String time : timeChoices) {
             dropdown.add(time);
         }
 
-        for (Component comp : guiComponents) {
-            frame.add(comp);
+		Component[] componentsList = comp.getComponentsList();
+        for (Component c : componentsList) {
+            frame.add(c);
         }
 
         frame.setSize(380, 400);
@@ -90,42 +45,21 @@ public class GUI {
         frame.setLayout(null);
         frame.setVisible(true);
 
-        dropdown.select(timeChoice);
+        dropdown.select(notifyTime);
 
-        addComponentListeners(frame, dropdown, checkboxComponents);
-    }
+        addComponentListeners(frame, dropdown, comp.getCheckboxes(), notifyState);
+	}
 
-    /**
-     * Set the contents for each checkbox and add the checkbox components to a list.
-     *
-     * @param panel                 the container for holding GUI components
-     * @param checkboxParameters    the list of parameters to initialise the checkbox contents
-     *
-     * @return                      a list of the checkbox components
-     */
-    private static List<Checkbox> addCheckboxComponents(Panel panel, List<Object[]> checkboxParameters) {
-        List<Checkbox> checkboxComponents = new ArrayList<>();
-
-        for (Object[] param : checkboxParameters) {
-            String name = (String) param[0];
-            boolean checkboxState = (boolean) param[1];
-
-            Checkbox checkbox = new Checkbox(name, checkboxState);
-            panel.add(checkbox);
-            checkboxComponents.add(checkbox);
-        }
-        return checkboxComponents;
-    }
-
-    /**
-     * Add a window listener to the frame to close the program
-     * and add item listeners to the dropdown and checkbox components.
-     *
-     * @param frame                     the visual window that the GUI components appear on
-     * @param dropdown                  the dropdown list that a user can choose from
-     * @param checkboxComponents        the list of checkboxes that a user can check or uncheck
-     */
-    private static void addComponentListeners(Frame frame, Choice dropdown, List<Checkbox> checkboxComponents) {
+	/**
+	 * Add a window listener to the frame to close the program
+	 * and add item listeners to the dropdown and checkbox components.
+	 *
+	 * @param frame				the visual window that the GUI components appear on
+	 * @param dropdown			the dropdown list that a user can choose from
+	 * @param checkboxes		the list of checkboxes that a user can check or uncheck
+	 * @param notifyState		the checkbox state preferences
+	 */
+	private static void addComponentListeners(Frame frame, Choice dropdown, List<Checkbox> checkboxes, String notifyState) {
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -135,45 +69,154 @@ public class GUI {
         });
 
         dropdown.addItemListener(e -> {
-            timeChoice = (String) e.getItem();
+            String notifyTime = String.valueOf(e.getItem());
+			prefs.put("TIME", notifyTime);
         });
 
-        for (Checkbox checkbox : checkboxComponents) {
-            checkbox.addItemListener(e -> {
-                int indexOfEvent = checkboxComponents.indexOf(checkbox);
-                int eventChoiceSelected = e.getStateChange();
-                String eventChoiceString = String.valueOf(eventChoiceSelected);
-
-                eventChoices.set(indexOfEvent, eventChoiceString);
+		List<String> stateList = getStateList(notifyState);
+        for (Checkbox c : checkboxes) {
+            c.addItemListener(e -> {
+				String state = String.valueOf(e.getStateChange());
+                int indexOfEvent = checkboxes.indexOf(c);
+				stateList.set(indexOfEvent, state);
+				
+				String stateString = getStateString(stateList);
+				prefs.put("STATE", stateString);
             });
         }
     }
 
-    /**
-     * Set the contents and visual bounds for each GUI component.
-     *
-     * @param panel     the container for holding GUI components
-     * @return          a component list of the GUI components
-     */
-    private static Component[] loadGUIComponents(Panel panel) {
-        Label subtitle = new Label("GW2 Event Notifications");
+	/**
+	 * Set the contents and visual bounds for each GUI component
+	 * and add them to the GUIComponents class.
+	 *
+	 * @param panel					the container for holding GUI components
+	 * @param timetable		the list of events with name, location, and projected event times
+	 * @param notifyState			the checkbox state preferences
+	 *
+	 * @return						the GUIComponents class with the prepared GUI components
+	 */
+    private static GUIComponents loadGUIComponents(Panel panel, List<Timetable> timetable, String notifyState) {
+        Label title = new Label("GW2 Event Notifications");
+		Label subtitle = new Label("Enable notifications per event:");
         Label leftText = new Label("Send a reminder");
         Label rightText = new Label("minutes before the next world boss");
         Choice dropdown = new Choice();
-        Label scrollWindowText = new Label("Enable notifications per event:");
+		
+		List<Checkbox> checkboxes = loadCheckboxComponents(panel, timetable, notifyState);
 
         panel.setLayout(new GridLayout(0, 1));
         ScrollPane scrollWindow = new ScrollPane();
         scrollWindow.add(panel);
         scrollWindow.setSize(panel.getSize());
 
-        subtitle.setBounds(15, 45, 300, 30);
+        title.setBounds(15, 45, 300, 30);
+		subtitle.setBounds(15, 140, 300, 30);
         leftText.setBounds(15, 90, 100, 20);
         rightText.setBounds(175, 90, 300, 20);
         dropdown.setBounds(115, 90, 50, 50);
-        scrollWindowText.setBounds(15, 140, 300, 30);
         scrollWindow.setBounds(15, 170, 350, 200);
 
-        return new Component[]{subtitle, leftText, rightText, dropdown, scrollWindowText, scrollWindow};
+        return new GUIComponents(title, subtitle, leftText, rightText, dropdown, scrollWindow, checkboxes);
     }
+
+	/**
+	 * Set the contents for each checkbox and add the checkbox components to a list.
+	 *
+	 * @param panel					the container for holding GUI components
+	 * @param timetable				the list of events with name, location, and projected event times
+	 * @param notifyState			the checkbox state preferences
+	 *
+	 * @return						a list of the checkbox components
+	 */
+	private static List<Checkbox> loadCheckboxComponents(Panel panel, List<Timetable> timetable, String notifyState) {
+		List<Parameters> checkboxParams = getCheckboxParameters(timetable, notifyState);
+		List<Checkbox> checkboxComponents = new ArrayList<>();
+		
+		for (Parameters p : checkboxParams) {
+			Checkbox checkbox = new Checkbox(p.getName(), p.getState());
+			panel.add(checkbox);
+			checkboxComponents.add(checkbox);
+		}
+		return checkboxComponents;
+	}
+
+	/**
+	 * Get the name string and boolean state used for initialising checkbox contents
+	 * based on the event name and whether notifications are enabled.
+	 *
+	 * @param timetable				the list of events with name, location, and projected event times
+	 * @param notifyState			the checkbox state preferences
+	 * @return						a list of checkbox parameters with the event name and notification state
+	 */
+	private static List<Parameters> getCheckboxParameters(List<Timetable> timetable, String notifyState) {
+		List<String> stateList = getStateList(notifyState);
+		List<Parameters> checkboxParams = new ArrayList<>();
+
+		for (Timetable event : timetable) {
+			String eventName = event.getName();
+			String eventLocation = event.getLocation();
+			
+			int indexOfEvent = timetable.indexOf(event);
+			String state = stateList.get(indexOfEvent);
+			boolean checkboxState = state.equals("1");
+			
+			if (eventName.equals("Ley-Line Anomaly")) {
+				eventName = eventName + " (" + eventLocation + ")";
+			}
+			
+			Parameters eventParameters = new Parameters(eventName, checkboxState);
+			checkboxParams.add(eventParameters);
+		}
+		return checkboxParams;
+    }
+
+	/**
+	 * Separate each character in the state preferences string and add it to a list.
+	 *
+	 * @param notifyState		the state preferences string
+	 * @return					a list of state preferences
+	 */
+	private static List<String> getStateList(String notifyState) {
+		List<String> stateList = new ArrayList<>();
+		for (int i = 0; i < notifyState.length(); i++) {
+			String state = String.valueOf(notifyState.charAt(i));
+			stateList.add(state);
+		}
+		return stateList;
+	}
+
+	/**
+	 * Put all checkbox states into one string.
+	 *
+	 * @param stateList		the checkbox state list
+	 * @return				a string of state preferences
+	 */
+	private static String getStateString(List<String> stateList) {
+		StringBuilder notifyState = new StringBuilder();
+		
+		for (String s : stateList) {
+			notifyState.append(s);
+		}
+		return String.valueOf(notifyState);
+	}
+
+	/**
+	 * Get the default or saved time preference from the Windows Registry.
+	 *
+	 * @return		the time preference string
+	 */
+	public static String getNotifyTime() {
+		return prefs.get("TIME", "10");
+	}
+
+	/**
+	 * Get the default or saved state preferences from the Windows Registry.
+	 *
+	 * @param defaultPreferences		the default preferences based on the number of events
+	 * @return							the state preferences string
+	 */
+	public static String getNotifyState(String defaultPreferences) {
+		return prefs.get("STATE", defaultPreferences);
+	}
 }
